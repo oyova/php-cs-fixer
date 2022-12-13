ef153983a34525d2ec1730dd81b5c4172aa4e514
Ryan Whitman <ryanawhitman@gmail.com>
2022-12-13 14:14:55 -0500

    Remove docs

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,3 @@
 <?php
 
-/**
- * This file is referenced in README.md as being an example of usage.
- */
-
-return (new Oyova\PhpCsFixer\Setup(__FILE__))
-    // ->in(string|array)
-    // ->exclude(string|array)
-    // ->notName(string|array)
-    ->getConfig();
+return (new Oyova\PhpCsFixer\Setup(__FILE__))->getConfig();
