487fdb90335f451b58b0df6eb6b1f42a37cb540f
Ryan Whitman <ryanawhitman@gmail.com>
2022-12-14 09:38:51 -0500

    String contains - adjust for php 7.4

--- src/Fixers/SpaceInsideParenthesisFixer.php
+++ src/Fixers/SpaceInsideParenthesisFixer.php
@@ -16,6 +16,11 @@ final class SpaceInsideParenthesisFixer extends AbstractFixer
 
     protected $singleLineWhitespaceOptions = " \t";
 
+    protected function str_contains(string $haystack, string $needle): bool
+    {
+        return $needle !== '' && mb_strpos($haystack, $needle) !== false;
+    }
+
     public function getDefinition(): FixerDefinitionInterface
     {
         return new FixerDefinition(
@@ -104,16 +109,14 @@ final class SpaceInsideParenthesisFixer extends AbstractFixer
     ): void {
         // add single-line whitespace before )
         if (
-            ! $tokens[$end - 1]->isWhitespace($this->singleLineWhitespaceOptions) &&
-            ! str_contains($tokens[$end - 1]->getContent(), "\n")
+            ! $tokens[$end - 1]->isWhitespace($this->singleLineWhitespaceOptions) && ! $this->str_contains($tokens[$end - 1]->getContent(), "\n")
         ) {
             $tokens->ensureWhitespaceAtIndex($end, 0, ' ');
         }
 
         // add single-line whitespace after (
         if (
-            ! $tokens[$start + 1]->isWhitespace($this->singleLineWhitespaceOptions) &&
-            ! str_contains($tokens[$start + 1]->getContent(), "\n")
+            ! $tokens[$start + 1]->isWhitespace($this->singleLineWhitespaceOptions) && ! $this->str_contains($tokens[$start + 1]->getContent(), "\n")
         ) {
             $tokens->ensureWhitespaceAtIndex($start, 1, ' ');
         }
